02b2dc6d497e5a0f835371c9f6768fa0e292b9c8
Thomas Peterson <tap6@email.sc.edu>
2024-10-24 12:39:36 -0400

    First Implementation

---
+++ react-layout/src/Layout.js
@@ -0,0 +1,15 @@
+import {Outlet, Link} from "react-router-dom";
+
+const Layout = () => {
+    return (
+        <>
+            <p>This is my header</p>
+
+            <Outlet />
+
+            <p>This is my footer</p>
+        </>
+    );
+};
+
+export default Layout;
